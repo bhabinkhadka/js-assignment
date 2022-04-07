cace6c1c3be3b2d2b3b5f0ac8a7a0b3c31026152
bhabin <bhabinkh@gmail.com>
2022-04-07 23:19:48 +0545

    js assigment

---
+++ main.js
@@ -0,0 +1,851 @@
+const users = [
+    {
+        id: 1,
+        email: "michael.lawson@reqres.in",
+        first_name: "Michael",
+        last_name: "Lawson",
+        avatar: "https://reqres.in/img/faces/7-image.jpg",
+    },
+    {
+        id: 2,
+        email: "lindsay.ferguson@reqres.in",
+        first_name: "Lindsay",
+        last_name: "Ferguson",
+        avatar: "https://reqres.in/img/faces/8-image.jpg",
+    },
+    {
+        id: 3,
+        email: "tobias.funke@reqres.in",
+        first_name: "Tobias",
+        last_name: "Funke",
+        avatar: "https://reqres.in/img/faces/9-image.jpg",
+    },
+    {
+        id: 4,
+        email: "byron.fields@reqres.in",
+        first_name: "Byron",
+        last_name: "Fields",
+        avatar: "https://reqres.in/img/faces/10-image.jpg",
+    },
+    {
+        id: 5,
+        email: "george.edwards@reqres.in",
+        first_name: "George",
+        last_name: "Edwards",
+        avatar: "https://reqres.in/img/faces/11-image.jpg",
+    },
+    {
+        id: 6,
+        email: "rachel.howell@reqres.in",
+        first_name: "Rachel",
+        last_name: "Howell",
+        avatar: "https://reqres.in/img/faces/12-image.jpg",
+    },
+];
+
+const messages = [
+    {
+        userId: 1,
+        id: 1,
+        message: "delectus aut autem",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 2,
+        message: "quis ut nam facilis et officia qui",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 3,
+        message: "fugiat veniam minus",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 4,
+        message: "et porro tempora",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 5,
+        message: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 6,
+        message: "qui ullam ratione quibusdam voluptatem quia omnis",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 7,
+        message: "illo expedita consequatur quia in",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 8,
+        message: "quo adipisci enim quam ut ab",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 9,
+        message: "molestiae perspiciatis ipsa",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 10,
+        message: "illo est ratione doloremque quia maiores aut",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 11,
+        message: "vero rerum temporibus dolor",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 12,
+        message: "ipsa repellendus fugit nisi",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 13,
+        message: "et doloremque nulla",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 14,
+        message: "repellendus sunt dolores architecto voluptatum",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 15,
+        message: "ab voluptatum amet voluptas",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 16,
+        message: "accusamus eos facilis sint et aut voluptatem",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 17,
+        message: "quo laboriosam deleniti aut qui",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 18,
+        message: "dolorum est consequatur ea mollitia in culpa",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 19,
+        message: "molestiae ipsa aut voluptatibus pariatur dolor nihil",
+        mine: false,
+    },
+    {
+        userId: 1,
+        id: 20,
+        message: "ullam nobis libero sapiente ad optio sint",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 21,
+        message: "suscipit repellat esse quibusdam voluptatem incidunt",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 22,
+        message: "distinctio vitae autem nihil ut molestias quo",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 23,
+        message: "et itaque necessitatibus maxime molestiae qui quas velit",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 24,
+        message: "adipisci non ad dicta qui amet quaerat doloribus ea",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 25,
+        message: "voluptas quo tenetur perspiciatis explicabo natus",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 26,
+        message: "aliquam aut quasi",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 27,
+        message: "veritatis pariatur delectus",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 28,
+        message: "nesciunt totam sit blanditiis sit",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 29,
+        message: "laborum aut in quam",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 30,
+        message: "nemo perspiciatis repellat ut dolor libero co",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 31,
+        message: "repudiandae totam in est sint facere fuga",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 32,
+        message: "earum doloribus ea doloremque quis",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 33,
+        message: "sint sit aut vero",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 34,
+        message: "porro aut necessitatibus eaque distinctio",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 35,
+        message: "repellendus veritatis molestias dicta incidunt",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 36,
+        message: "excepturi deleniti adipisci voluptatem et neque optio illum ad",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 37,
+        message: "sunt cum tempora",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 38,
+        message: "totam quia non",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 39,
+        message: "doloremque quibusdam asperiores libero corrupti illum qui omnis",
+        mine: false,
+    },
+    {
+        userId: 2,
+        id: 40,
+        message: "totam atque quo nesciunt",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 41,
+        message: "aliquid amet impedit consequatur aspernatur placeat eaqu",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 42,
+        message: "rerum perferendis error quia ut eveniet",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 43,
+        message: "tempore ut sint quis recusandae",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 44,
+        message: "cum debitis quis accusamus doloremque ipsa natus sapiente omnis",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 45,
+        message: "velit soluta adipisci molestias reiciendis harum",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 46,
+        message: "vel voluptatem repellat nihil placeat corporis",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 47,
+        message: "nam qui rerum fugiat accusamus",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 48,
+        message: "sit reprehenderit omnis quia",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 49,
+        message: "ut necessitatibus aut maiores debitis officia blanditi",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 50,
+        message: "cupiditate necessitatibus ullam aut quis dolor voluptate",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 51,
+        message: "distinctio exercitationem ab doloribus",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 52,
+        message: "nesciunt dolorum quis recusandae ad pariatur ratione",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 53,
+        message: "qui labore est occaecati recusandae aliquid quam",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 54,
+        message: "quis et est ut voluptate quam dolor",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 55,
+        message: "voluptatum omnis minima qui occaecati provident nulla",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 56,
+        message: "deleniti ea temporibus enim",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 57,
+        message: "pariatur et magnam ea doloribus similique voluptatem rerum quia",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 58,
+        message: "est dicta totam qui explicabo doloribus qui dignissimos",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 59,
+        message: "perspiciatis velit id laborum placeat iusto et aliquam odio",
+        mine: false,
+    },
+    {
+        userId: 3,
+        id: 60,
+        message: "et sequi qui architecto ut adipisci",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 61,
+        message: "odit optio omnis qui sunt",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 62,
+        message: "et placeat et tempore aspernatur sint numquam",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 63,
+        message: "doloremque aut dolores quidem fuga qui nulla",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 64,
+        message: "voluptas consequatur qui ut quia magnam nemo esse",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 65,
+        message: "fugiat pariatur ratione ut asperiores necessitatibus magni",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 66,
+        message: "rerum eum molestias autem voluptatum sit optio",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 67,
+        message: "quia voluptatibus voluptatem quos similique maiores repellat",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 68,
+        message: "aut id perspiciatis voluptatem iusto",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 69,
+        message: "doloribus sint dolorum ab adipisci itaque dignissimos",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 70,
+        message: "ut sequi accusantium et mollitia delectus sunt",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 71,
+        message: "aut velit saepe ullam",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 72,
+        message: "praesentium facilis facere quis harum voluptatibu",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 73,
+        message: "sint amet quia totam corporis qui exercitationem commodi",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 74,
+        message: "expedita tempore nobis eveniet laborum maiores",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 75,
+        message: "occaecati adipisci est possimus totam",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 76,
+        message: "sequi dolorem sed",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 77,
+        message: "maiores aut nesciunt delectus exercitationem vel assumenda",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 78,
+        message: "reiciendis est magnam amet nemo iste recusandae impedit quaerat",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 79,
+        message: "eum ipsa maxime ut",
+        mine: false,
+    },
+    {
+        userId: 4,
+        id: 80,
+        message: "tempore molestias dolores rerum sequi voluptates ipsum",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 81,
+        message: "suscipit qui totam",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 82,
+        message: "voluptates eum voluptas et dicta",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 83,
+        message: "quidem at rerum quis ex aut sit quam",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 84,
+        message: "sunt veritatis ut voluptate",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 85,
+        message: "et quia ad iste a",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 86,
+        message: "incidunt ut saepe autem",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 87,
+        message: "laudantium quae eligendi consequatur quia et vero autem",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 88,
+        message: "vitae aut excepturi laboriosam sint aliquam et et accusantium",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 89,
+        message: "sequi ut omnis et",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 90,
+        message: "molestiae nisi accusantium tenetur dolorem et",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 91,
+        message: "nulla quis consequatur saepe qui id expedita",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 92,
+        message: "in omnis laboriosam",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 93,
+        message: "odio iure consequatur molestiae quibusdam necessitatibus",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 94,
+        message: "facilis modi saepe mollitia",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 95,
+        message: "vel nihil et molestiae iusto assumenda nemo quo ut",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 96,
+        message: "nobis suscipit ducimus enim asperiores voluptas",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 97,
+        message: "dolorum laboriosam eos qui iure aliquam",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 98,
+        message: "debitis accusantium ut quo facilis nihil quis sapiente",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 99,
+        message: "neque voluptates ratione",
+        mine: false,
+    },
+    {
+        userId: 5,
+        id: 100,
+        message: "excepturi a et neque qui expedita vel voluptate",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 101,
+        message: "explicabo enim cumque porro aperiam occaecati minima",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 102,
+        message: "sed ab consequatur",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 103,
+        message: "non sunt delectus illo nulla tenetur enim omnis",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 104,
+        message: "excepturi non laudantium quo",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 105,
+        message: "totam quia dolorem et illum repellat voluptas optio",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 106,
+        message: "ad illo quis voluptatem temporibus",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 107,
+        message: "praesentium facilis omnis laudantium fugit ad iusto nihil",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 108,
+        message: "a eos eaque nihil et exercitationem incidunt delectus",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 109,
+        message: "autem temporibus harum quisquam in culpa",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 110,
+        message: "aut aut ea corporis",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 111,
+        message: "magni accusantium labore et id quis provident",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 112,
+        message: "consectetur impedit quisquam qui deserunt non rerum",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 113,
+        message: "quia atque aliquam sunt impedit voluptatum rerum assumenda nisi",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 114,
+        message: "cupiditate quos possimus corporis quisquam exercitationem beatae",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 115,
+        message: "sed et ea eum",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 116,
+        message: "ipsa dolores vel facilis ut",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 117,
+        message: "sequi quae est et qui qui eveniet asperiores",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 118,
+        message: "quia modi consequatur vero fugiat",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 119,
+        message: "corporis ducimus ea perspiciatis iste",
+        mine: false,
+    },
+    {
+        userId: 6,
+        id: 120,
+        message: "dolorem laboriosam vel voluptas et aliquam quasi",
+        mine: false,
+    },
+];
+
+let selectedUser = 1;
+const userListing = document.querySelector(".users-listing");
+const messageListing = document.querySelector(".message-listing");
+const inputMessage = document.querySelector("#input-message");
+const inputSearch = document.querySelector("#input-search");
+
+const renderUser = (arr) => {
+    userListing.innerHTML = ``;
+    for (let i = 0; i < arr.length; i++) {
+        userListing.innerHTML += `
+      <div data-id="${arr[i].id}" class="user-item">
+        <img src="${arr[i].avatar}" alt="" />
+        <div class="details">
+          <h1>${arr[i].first_name} ${arr[i].last_name}</h1>
+          <p>${arr[i].email}</p>
+        </div>
+      </div>
+    `;
+    }
+};
+
+renderUser(users);
+
+inputSearch.addEventListener("keyup", (e) => {
+    const _val = e.target.value;
+    const _users = users.filter((el) => {
+        const _nM = el.first_name + " " + el.last_name;
+        return _nM.toLowerCase().includes(_val.toLowerCase());
+    });
+
+    renderUser(_users || []);
+});
+
+const sendMessage = (msg) => {
+    const payload = {
+        userId: selectedUser,
+        message: msg,
+        mine: true,
+        id: messages.length + 1,
+    };
+    messages.push(payload);
+    showMessage();
+};
+
+inputMessage.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+        sendMessage(e.target.value);
+        e.target.value = "";
+    }
+});
+
+const showMessage = () => {
+    messageListing.innerHTML = ``;
+    for (let i = 0; i < messages.length; i++) {
+        const el = messages[i];
+        if (el.userId !== selectedUser) continue;
+        const userDetail = users?.find((el) => el.id === selectedUser);
+        messageListing.innerHTML += `
+      <div class="message-item ${el.mine ? "right" : ""}">
+        <div class="details">
+          <h1>${el?.mine
+                ? "You"
+                : userDetail?.first_name + " " + userDetail?.last_name
+            } </h1>
+          <p>
+            ${el.message}
+          </p>
+        </div>
+      </div>
+    `;
+    }
+    messageListing.scrollTop = messageListing.scrollHeight;
+};
+
+userListing.addEventListener("click", (e) => {
+    if (e.target.classList.contains("user-item")) {
+        const userId = e.target.dataset.id;
+        selectedUser = parseInt(userId);
+        showMessage();
+    }
+});
+
+showMessage(1);
